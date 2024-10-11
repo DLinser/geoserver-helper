198a32add75ba5ea571b91364b4beb2e7452ef82
linyongxin <linyongxin@ellipspace.com>
2024-10-11 16:26:30 +0800

    补充上次提交

--- lib/interface/interface.ts
+++ lib/interface/interface.ts
@@ -4,6 +4,7 @@ export type { IWorkspace } from './workspace';
 export type { INamespaces } from "./namespaces";
 export type { IDatastore } from './datastore';
 export type { ISystem } from './system';
+export type { IResource } from './resource';
 export type { IStyle } from './style';
 export type { IWms } from './wms';
 export type { IWps } from './wps';
